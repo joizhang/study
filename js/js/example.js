bd243c28d2ac77625fe3104bb06afcdd1f53b0eb
Joizhang <zxlwhom@outlook.com>
2015-08-13 22:02:17 +0800

    chapter7

--- js/js/example.js
+++ js/js/example.js
@@ -1,4 +1,20 @@
 window.onload = function() {
-    var testdiv = document.getElementById("testdiv");
-    testdiv.innerHTML = "<p>This is <em>my</em> content.</p>";
-  }
+  var para = document.createElement("p");
+  var txt1 = document.createTextNode("This is ");
+  var emphasis = document.createElement("em");
+  var txt2 = document.createTextNode("my");
+  var txt3 = document.createTextNode(" content.");
+  para.appendChild(txt1);
+  emphasis.appendChild(txt2);
+  para.appendChild(emphasis);
+  para.appendChild(txt3);
+  var divtest = document.getElementById("testdiv");
+  divtest.appendChild(para);
+}
+/*window.onload = function() {
+  var para = document.createElement("p");
+  var txt = document.createTextNode("Hello World!");
+  para.appendChild(txt);
+  var testdiv = document.getElementById("testdiv");
+  testdiv.appendChild(para);
+}*/
