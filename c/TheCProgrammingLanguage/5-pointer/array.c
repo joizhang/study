4e19cb24125e9205c4281470f9f90d8eb4dd23f0
Zhang Xinlin <xinlin.zhang@garena.com>
2022-09-14 15:47:57 +0800

    refactor: rename The C Programming Language directory and its subdirectory

--- c/TheCProgrammingLanguage/5-pointer/array.c
+++ c/TheCProgrammingLanguage/5-pointer/array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
 	int a = 3;
 	int b = 2;
@@ -21,4 +21,5 @@ void main()
 		p++;
 	}
 	printf("-----------------------------------------------------------------\n");
+	return 0;
 }
